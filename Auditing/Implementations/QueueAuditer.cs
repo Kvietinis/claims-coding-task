becce4fa59965d842f9816e5e5dd20c984255a85
Ignas Glemža <Ignas.Glemza@strongpoint.com>
2024-02-05 01:30:02 +0200

    migrate to .net 8

---
+++ Auditing/Implementations/QueueAuditer.cs
@@ -0,0 +1,24 @@
+﻿using Claims.Auditing.Abstractions;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Claims.Auditing.Implementations
+{
+    public class QueueAuditer : IAuditer
+    {
+        //ConcurrentQueue<T> Class
+        public Task AuditClaim(string id, string httpRequestType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task AuditCover(string id, string httpRequestType)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
