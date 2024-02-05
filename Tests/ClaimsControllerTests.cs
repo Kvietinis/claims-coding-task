edfa33ec225cd18e0e302bd967e03fb1cb01ee0f
Ignas Glemža <Ignas.Glemza@strongpoint.com>
2024-02-05 02:57:01 +0200

    small fix

--- Tests/ClaimsControllerTests.cs
+++ Tests/ClaimsControllerTests.cs
@@ -1,25 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc.Testing;
+﻿using Claims.RestApi;
+using Microsoft.AspNetCore.Mvc.Testing;
 using Xunit;
 
 namespace Claims.Tests
 {
     public class ClaimsControllerTests
     {
-        //[Fact]
-        //public async Task Get_Claims()
-        //{
-        //    var application = new WebApplicationFactory<Program>()
-        //        .WithWebHostBuilder(_ =>
-        //        {});
+        [Fact]
+        public async Task Get_Claims()
+        {
+            var application = new WebApplicationFactory<Program>()
+                .WithWebHostBuilder(_ =>
+                { });
 
-        //    var client = application.CreateClient();
+            var client = application.CreateClient();
 
-        //    var response = await client.GetAsync("/Claims");
+            var response = await client.GetAsync("/Claims");
 
-        //    response.EnsureSuccessStatusCode();
+            response.EnsureSuccessStatusCode();
 
-        //    //TODO: Apart from ensuring 200 OK being returned, what else can be asserted?
-        //}
+            //TODO: Apart from ensuring 200 OK being returned, what else can be asserted?
+        }
 
     }
 }
